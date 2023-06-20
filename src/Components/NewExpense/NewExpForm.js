1a8b2fb43d4910609e49638a30dbdc9d0565987a
xpknight <xpknight@gmail.com>
2023-06-20 17:11:44 +0530

    submit button functinality added and ui udpated

--- src/Components/NewExpense/NewExpForm.js
+++ src/Components/NewExpense/NewExpForm.js
@@ -2,26 +2,43 @@ import React, { useState } from 'react';
 
 import "./NewExpForm.css"
 const NewExpForm = () => {
-    const [newTitle, setNewTitle] = useState("");
-    const [newDate, setNewDate] = useState("");
-    const [newPrice, setNewPrice] = useState("");
+    // const [newTitle, setNewTitle] = useState("");
+    // const [newDate, setNewDate] = useState("");
+    // const [newPrice, setNewPrice] = useState("");
+
+    const [newData, setNewData] = useState({
+        title: "",
+        date: "",
+        price: ""
+    });
 
     const setTitleHandler = (event) => {
-        setNewTitle(event.target.value);
-        console.log(newTitle);
+        setNewData((prevState) => {
+            return { ...prevState, title: event.target.value }
+        })
     }
     const setDateHandler = (event) => {
-        setNewDate(event.target.value);
-        console.log(newDate);
+        setNewData((prevState) => {
+            return { ...prevState, date: event.target.value }
+        })
     }
     const setPriceHandler = (event) => {
-        setNewPrice(event.target.value);
-        console.log(newPrice);
+        setNewData((prevState) => {
+            return { ...prevState, price: event.target.value }
+        })
     }
 
-
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+        const newExpenseData = {
+            title: newData.title,
+            date: newData.date,
+            price: newData.price
+        }
+        console.log(newExpenseData);
+    }
     return (
-        <form className="newExpense_form">
+        <form className="newExpense_form" onSubmit={formSubmitHandler}>
             <div className="expenses-filter__control">
                 <div className="expenses-filter">
                     <label for="title">Title </label>
@@ -29,7 +46,7 @@ const NewExpForm = () => {
                 </div>
                 <div className="expenses-filter">
                     <label for="Price">Price </label>
-                    <input type="number" min='0.5' step='10' name="price" onChange={setPriceHandler} />
+                    <input type="number" min='0' step='10' name="price" onChange={setPriceHandler} />
                 </div>
                 <div className="expenses-filter">
                     <label for="Date">Date </label>
